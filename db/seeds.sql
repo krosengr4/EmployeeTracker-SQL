8d1861bd0bcb500959e1d36c05837faff98fe9b8
krosengr4 <rosenkev4@gmail.com>
2023-11-10 14:46:05 -0600

    Finished seeds.sql with Brooklyn99 Character theme

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,23 +1,33 @@
 -- ! seeds.sql file fills db with data of employees who already exist.
 
--- ! BROOKLYN 99 THEME *******
+-- ! ******* BROOKLYN 99 THEME *******
 
 INSERT INTO department (department_name)
 VALUES
-('Police Captain'),
-('Sargent')
-('Detective'),
-('Captains Assistant');
+('Police Force'),
+('Office Employees'),
+('Entertainment hires');
 
 INSERT INTO job (title, salary, department_id)
 VALUES
-(''),
-('');
+('Police Captain', 120,000, 1),
+('Sargent', 80000, 1),
+('Detective', 52000, 1),
+('Captains Assistant', 30000, 2),
+('Comedic Relief', 18000, 3);
 
-INSERT INTO employee (first_name, last_name, job_id, manager_id)
+INSERT INTO employee (first_name, last_name, job_id)
 VALUES
-(''),
-('');
+('Jake', 'Peralta', 03),
+('Charles', 'Boyle', 03),
+('Amy', 'Santiago', 03),
+('Rosa', 'Diaz', 03),
+('Terry', 'Jeffords', 02),
+('Gina', 'Linetti', 04),
+('Michael', 'Hitchcock', 05),
+('Norm', 'Scully', 05),
+('Raymond', 'Holt', 01);
+
 
 
 
