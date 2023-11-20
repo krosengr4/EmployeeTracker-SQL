a0983b77fffbd83c6b31aac117db803157696371
krosengr4 <rosenkev4@gmail.com>
2023-11-20 01:09:35 -0600

    installed what I needed to get inquirer and mysql to work

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+// Import dependencies
 const inquirer = require('inquirer');
 const mySql = require('mysql');
-const app = express();
+// const app = express();
 
 // Create connection to MySQL
 const connection = mySql.createConnection({
@@ -8,7 +9,8 @@ const connection = mySql.createConnection({
     port: 3306,
     database: 'employee_db',
     user: 'root',
-    password: '611854kr', //<--- Add MySQL password here
+    //TODO: Add "MySQL" password below
+    password: '611854kr',
 });
 
 // Array of choices the user can make
@@ -31,76 +33,102 @@ connection.connect(error => {
 });
 
 // This function runs inquirer.prompt, and handles the suser selection
-const startPropmt = async () => {
-    try {
-        let response = await inquirer.prompt({
-            name: 'action', 
-            type: 'list',
-            message: 'Hello! What would you like to do?',
-            choices: userChoices
-        }); 
-        // switch function to handle the user choice, calls appropriate function
+const startPropmt = () => {
+    inquirer.prompt({
+        message: 'What would you like to do?',
+        name: 'action', 
+        type: 'list',
+        choices: userChoices,
+    })
+    .then(response => {
         switch (response.action) {
             case 'View All Employees': viewEmployees();
-            break; 
-            
-            case 'Add Employee': addEmployee();
-            break;
-
-            case 'Update Employee Info': updateEmployee();
-            break;
-
-            case 'View All Departments': viewDepartments();
-            break;
-
-            case 'Add a Department': addDepartment();
-            break;
-
-            case 'View All Jobs': viewJobs();
-            break;
-
-            case 'Add a Job': addJob();
-            break;
-
-            case "Exit": connection.end();
-            break;
-        }
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-// Function to view all employees
-const viewEmployees = async () => {
-    console.log('You chose to View All Employees');
+                break; 
+                
+                case 'Add Employee': addEmployee();
+                break;
+    
+                case 'Update Employee Info': updateEmployee();
+                break;
+    
+                case 'View All Departments': viewDepartments();
+                break;
+    
+                case 'Add a Department': addDepartment();
+                break;
+    
+                case 'View All Jobs': viewJobs();
+                break;
+    
+                case 'Add a Job': addJob();
+                break;
+    
+                case "Exit": connection.end();
+                break;
+    
+                default: connection.end();
+            }
+    });
 };
 
-// Function to add an employee
-const addEmployee = async () => {
-    console.log('You chose to Add an Employee');
-};
+    
+    // Function to view all employees
+    const viewEmployees = () => {
+        console.log('You chose to View All Employees');
+        
+        connection.query('SELECT * FROM employee;',
+        function (err, res) {
+            if (err) throw err;
+            console.table(res);
+            startPropmt();
+        })
+    };
+    
+    // Function to view all departments
+    const viewDepartments = async () => {
+        console.log('You chose to View All Departments');
+    };
 
-//Function to update an employee
-const updateEmployee = async () => {
-    console.log('You chose to Update Employee Information');
-};
+    // Function to view all jobs
+    const viewJobs = async () => {
+        console.log('You chose to View All Jobs');
+    };
+    
+    //Function to update an employee
+    const updateEmployee = async () => {
+        console.log('You chose to Update Employee Information');
+    };
 
-// Function to view all departments
-const viewDepartments = async () => {
-    console.log('You chose to View All Departments');
-};
+    // Function to add an employee
+    const addEmployee = async () => {
+        console.log('You chose to Add an Employee');
+    };
+    
+    
+    
+    // Function to add a department
+    const addDepartment = async () => {
+        console.log('You chose to Add a Department');
+    };
+    
+    
+    // Function to add a job
+    const addJob = async () => {
+        console.log('You chose to Add a Job');
+    };
 
-// Function to add a department
-const addDepartment = async () => {
-    console.log('You chose to Add a Department');
-};
 
-// Function to view all jobs
-const viewJobs = async () => {
-    console.log('You chose to View All Jobs');
-};
 
-// Function to add a job
-const addJob = async () => {
-    console.log('You chose to Add a Job');
-};
+    //         // switch function to handle the user choice, calls appropriate function
+    //     try {
+    //         let response = await inquirer.prompt({
+    //             name: 'action', 
+    //             type: 'list',
+    //             message: 'Hello! What would you like to do?',
+    //             choices: userChoices
+    //         }); 
+    //         switch (response.action) {
+                
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
